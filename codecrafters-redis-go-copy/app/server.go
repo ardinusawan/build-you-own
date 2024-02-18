c910fbfb75a8d2313b766fd5faa1a82bf1c285bd
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2024-02-18 18:00:17 +0700

    init codecrafters-redis-go

---
+++ codecrafters-redis-go-copy/app/server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func handleConnection(conn net.Conn) {
+	fmt.Println("Handling new connection")
+	// defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("received %d bytes\n", n)
+	fmt.Printf("received the following data: %s", string(buf[:n]))
+	if isPing(buf[:n]) {
+		pong(conn)
+	}
+}
+
+func pong(conn net.Conn) {
+	pong := []byte("+PONG\r\n")
+	n, err := conn.Write(pong)
+	if err != nil {
+		fmt.Println("Error responsding to ping:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("sent %d bytes", n)
+	fmt.Printf("sent the following data: %s", string(pong))
+}
+
+func isPing(command []byte) bool {
+	ping := "*1\r\n$4\r\nping\r\n" // RESP
+	if string(command) == ping {
+		return true
+	}
+	return false
+}
+
+func main() {
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
+}
